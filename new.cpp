5222aba14c795c5418153ea0ea89984bf150086f
prashanth-2911 <kathulaprashanth6@gmail.com>
2025-03-05 18:59:32 +0530

    initial commit

---
+++ new.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+// #include<iomanip>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 9;
+    cout<<"a+b is: "<<(a+b)<<endl;
+    cout<<"a-b is: "<<(a-b)<<endl;
+    cout<<"a*b is: "<<(a*b)<<endl;
+    cout<<"a/b is: "<<(a/b)<<endl;
+    cout<<"a%b is: "<<(a%b)<<endl;
+    return 0;
+}
